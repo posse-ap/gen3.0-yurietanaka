752de8422de62c6fc509b7d10cfbff4fddce4f13
yurie tanaka <uri.t.213@gmail.com>
2022-11-12 16:44:34 +0900

    tweet機能追加

--- ph1-web app/assets/scripts/modal.js
+++ ph1-web app/assets/scripts/modal.js
@@ -2,10 +2,13 @@
 
 // モーダルが開く、選択できる
 const modal = document.querySelector('.js-modal');
-const modalOpenButton = document.querySelector('.js-modal-open-button');
-modalOpenButton.addEventListener('click', ()=> {
-    modal.classList.add('u-display-block');
+const modalOpenButtons = document.querySelectorAll('.js-modal-open-button');
+modalOpenButtons.forEach((modalOpenButton) => {
+    modalOpenButton.addEventListener('click', ()=> {
+        modal.classList.add('u-display-block');
+    })
 })
+
 // モーダルを閉じる
 const modalCloseButton = document.querySelector('.js-modal-close-button');
 modalCloseButton.addEventListener('click',()=>{
@@ -13,16 +16,22 @@ modalCloseButton.addEventListener('click',()=>{
     recordDown.classList.remove('u-display-flex');
     modalInner.classList.remove('u-display-hidden') ;
 })
-// どこを押してもモーダルが閉じる
-// const Overlay = document.querySelector('.js-overlay');
-// Overlay.addEventListener('click',()=> {
-//     modal.classList.remove('u-display-block');
-// })
+
+const tweetCheckBox = document.querySelector('.js-tweet-checkbox')
+const tweetArea = document.getElementById("js-tweet-area");
+const tweet = () => {
+    if (tweetCheckBox.checked) {
+      const tweetText = `${tweetArea.value}`;
+      window.open(`http://twitter.com/intent/tweet?&text=${tweetText}`);
+    }
+  }; //ツイート機能
+
 //記録投稿ボタンを押したとき＝ ローディング画面
 const modalInner = document.querySelector('.js-modal-inner');
 const record = document.querySelector('.js-button-record-done');
 const nowLoading = document.querySelector('.js-now-loading');
     record.addEventListener('click',()=>{
+        tweet();
     modalInner.classList.add('u-display-hidden')
     modalCloseButton.classList.add('u-display-hidden') 
     nowLoading.classList.add('u-display-block')
